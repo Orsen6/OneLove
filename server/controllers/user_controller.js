4b6f7840c1d12cf946a217af2af62e5cf72912b6
Volodymyr Hladkyi <vova.gladkiii@gmail.com>
2021-11-19 18:53:59 +0200

    img upload

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,11 +1,21 @@
 import userService from '../service/user_service.js';
+import {v4} from 'uuid';
+import path from 'path';
+
+const __dirname = path.resolve();
 
 
 class UserController{
     async registration (req, res, next) {
         try {
-            const {email, password, name, surname, age, gender, image, summary} = req.body;
-            const userData = await userService.registration({email, password, name, surname, age, gender, image, summary});
+            const {email, password, name, surname, age, gender, summary} = req.body;
+            const {image} = req.files;
+
+            let imageName = v4() + ".jpg";
+            await image.mv(path.resolve(__dirname, 'static', imageName));
+            const userData = await userService.registration(
+                {email, password, name, surname, age, gender, image: imageName, summary}
+            );
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
             return res.json(userData);
         } catch (error) {
--- server/index.js
+++ server/index.js
@@ -4,12 +4,14 @@ import sequelize from './db.js';
 import models from './models/models.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import fileUpload from 'express-fileupload';
 import router from './router/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(fileUpload({}));
 app.use(cookieParser());
 app.use(cors());
 app.use('/api', router);
