981dd5ef8352951f01b0d0dbea95845914f27fad
Orsen6 <orsenkul@gmail.com>
2021-11-22 12:59:17 +0200

    rega

---
+++ client/js/main.js
@@ -0,0 +1,26 @@
+window.addEventListener("DOMContentLoaded", function() {
+    let form = document.querySelector(".main-form_registration");
+    let pass = form.querySelectorAll("#password, #confirm-password");
+    let errorDiv = form.querySelector(".invalid-confirm");
+    $('.main-form_btn-continue').on("click",function(e){
+        e.preventDefault();
+    });
+    form.addEventListener("submit", function(e) {
+        let err = !(pass[0].value && (pass[0].value == pass[1].value))
+        pass[1].setAttribute("title", err ? errorDiv.innerHTML="Перевірте правильність пароля" : "");
+        err && e.preventDefault();
+    }, false);
+    pass[1].addEventListener("input", (e)=>{
+        let err = !(pass[0].value == pass[1].value)
+        pass[1].setAttribute("title", err ? "err" : "");
+    }, false)   
+})
+
+document.querySelector(".main-form_btn-continue").addEventListener("click", () => {
+    document.querySelector(".main-form_registration").classList.add("main-form_registration--invise");
+    document.querySelector(".main-form_about").classList.add("main-form_about--active");
+});
+submitForms = function(){
+    document.getElementById("form1").submit();
+    document.getElementById("form2").submit();
+}
