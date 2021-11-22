58c43b683ee155e2606ea4058900b191016d9afe
Orsen6 <orsenkul@gmail.com>
2021-11-22 15:00:28 +0200

    babass

--- client/js/send.js
+++ client/js/send.js
@@ -1,7 +1,5 @@
-document.querySelector("#form1").addEventListener("submit", () =>{
-    let formData = new FormData(document.forms.form1);
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:5000/api/registration");
-    xhr.send(formData);
-    xhr.onload = () => alert(xhr.response);
-})
+let formData = new FormData(document.forms.form1);
+let xhr = new XMLHttpRequest();
+xhr.open("POST", "http://localhost:5000/api/registration");
+xhr.send(formData);
+xhr.onload = () => alert(xhr.response);
