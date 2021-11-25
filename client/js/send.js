74a5b5e83f2ad210b40b0a74be05f5c851e22dba
Orsen6 <orsenkul@gmail.com>
2021-11-25 11:55:28 +0200

    123

--- client/js/send.js
+++ client/js/send.js
@@ -1,8 +1,36 @@
-document.querySelector("#form1").addEventListener("submit", () => {
-    let formData = new FormData(document.forms.form1);
-    console.log(formData);
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:5000/api/registration");
-    xhr.send(formData);
-    xhr.onload = () => alert(xhr.response);
+// document.querySelector("#form1").addEventListener("submit", () => {
+//     let formData = new FormData(document.forms.form1);
+//     console.log(formData);
+//     let xhr = new XMLHttpRequest();
+//     xhr.open("POST", "http://localhost:5000/api/registration");
+//     xhr.send(formData);
+//     xhr.onload = () => alert(xhr.response);
+// })
+let app = new Vue({
+    el: "#app",
+    data: {
+        email: "",
+        pass: "",
+        name: "",
+        surname: "",
+        age: "",
+        gender: "",
+        summary: "",
+        image: null
+    },
+    methods: {
+        async sendInfo(e) {
+             e.preventDefault();
+            const request = fetch("http://localhost:5000/api/registration", {
+                method: "POST",
+                email: this.email,
+                pass: this.pass,
+                name: this.name,
+                surname: this.surname,
+                age: this.age,
+                gender: this.gender,
+                summary: this.summary
+            })
+        }
+    }
 })
