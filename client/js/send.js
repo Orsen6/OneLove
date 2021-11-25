730b93290038179c6990da540c4994ac87eb16bc
Orsen6 <orsenkul@gmail.com>
2021-11-25 15:27:24 +0200

    123

--- client/js/send.js
+++ client/js/send.js
@@ -25,20 +25,20 @@ let app = new Vue({
         },
         async sendInfo(e) {
             e.preventDefault();
-            const data = new FormData();
-            data.append('image', this.image);
-            data.append('email', this.email);
-            data.append('password', this.password);
-            data.append('name', this.name);
-            data.append('surname', this.surname);
-            data.append('age', this.age);
-            data.append('gender', this.selectedGender);
-            data.append('summary', this.summary);
-            data.append('age', this.age);
+            // const data = new FormData();
+            // data.append('image', this.image);
+            // data.append('email', this.email);
+            // data.append('password', this.password);
+            // data.append('name', this.name);
+            // data.append('surname', this.surname);
+            // data.append('age', this.age);
+            // data.append('gender', this.selectedGender);
+            // data.append('summary', this.summary);
+            // data.append('age', this.age);
             const response = await fetch("http://localhost:5000/api/registration", {
                 method: "POST",
                 headers: {"Content-Type": "multipart/form-data"},
-                body: data
+                body: new FormData(form1),
                 // JSON.stringify({
                 //     email: this.email,
                 //     password: this.pass,
@@ -49,7 +49,7 @@ let app = new Vue({
                 //     summary: this.summary
                 // }),
             })
-            const data2 = await response.json();
+            const response = await response.json();
         }
     }
 })
