43d23f45d5b1148f0e374bf306908b4cabc7726f
Orsen6 <orsenkul@gmail.com>
2021-11-25 12:29:12 +0200

    123

--- client/js/send.js
+++ client/js/send.js
@@ -1,40 +1,40 @@
-document.querySelector("#form1").addEventListener("submit", () => {
-    let formData = new FormData(document.forms.form1);
-    console.log(formData);
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:5000/api/registration");
-    xhr.send(formData);
-    xhr.onload = () => alert(xhr.response);
-})
-// let app = new Vue({
-//     el: "#app",
-//     data: {
-//         email: "",
-//         password: "",
-//         name: "",
-//         surname: "",
-//         age: "",
-//         gender: "",
-//         summary: "",
-//         image: null
-//     },
-//     methods: {
-//         async sendInfo(e) {
-//             e.preventDefault();
-//             const response = await fetch("http://localhost:5000/api/registration", {
-//                 method: "POST",
-//                 headers: {"Content-Type": "application/json"},
-//                 body: JSON.stringify({
-//                     email: this.email,
-//                     pass: this.pass,
-//                     name: this.name,
-//                     surname: this.surname,
-//                     age: this.age,
-//                     gender: this.gender,
-//                     summary: this.summary
-//                 }),
-//             })
-//             const data = await response.json();
-//         }
-//     }
+// document.querySelector("#form1").addEventListener("submit", () => {
+//     let formData = new FormData(document.forms.form1);
+//     console.log(formData);
+//     let xhr = new XMLHttpRequest();
+//     xhr.open("POST", "http://localhost:5000/api/registration");
+//     xhr.send(formData);
+//     xhr.onload = () => alert(xhr.response);
 // })
+let app = new Vue({
+    el: "#app",
+    data: {
+        email: "",
+        password: "",
+        name: "",
+        surname: "",
+        age: "",
+        gender: "",
+        summary: "",
+        image: null
+    },
+    methods: {
+        async sendInfo(e) {
+            e.preventDefault();
+            const response = await fetch("http://localhost:5000/api/registration", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({
+                    email: this.email,
+                    password: this.pass,
+                    name: this.name,
+                    surname: this.surname,
+                    age: this.age,
+                    gender: this.gender,
+                    summary: this.summary
+                }),
+            })
+            const data = await response.json();
+        }
+    }
+})
