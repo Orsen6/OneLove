aed013b7ac808f699db89d7690086159b6c2f3e2
Orsen6 <orsenkul@gmail.com>
2021-11-25 14:51:17 +0200

    123

--- client/js/send.js
+++ client/js/send.js
@@ -14,27 +14,42 @@ let app = new Vue({
         name: "",
         surname: "",
         age: "",
-        gender: "",
+        selectedGender: [],
         summary: "",
-        image: null
+        image: ""
     },
     methods: {
+        uploadFile(event) {
+            this.image = event.target.files[0];
+            console.log(event);
+        },
         async sendInfo(e) {
             e.preventDefault();
+            const data = new FormData();
+            data.append('image', this.image);
+            data.append('email', this.email);
+            data.append('password', this.password);
+            data.append('name', this.name);
+            data.append('surname', this.surname);
+            data.append('age', this.age);
+            data.append('gender', this.selectedGender);
+            data.append('summary', this.summary);
+            data.append('age', this.age);
             const response = await fetch("http://localhost:5000/api/registration", {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({
-                    email: this.email,
-                    password: this.pass,
-                    name: this.name,
-                    surname: this.surname,
-                    age: this.age,
-                    gender: this.gender,
-                    summary: this.summary
-                }),
+                body: data
+                // JSON.stringify({
+                //     email: this.email,
+                //     password: this.pass,
+                //     name: this.name,
+                //     surname: this.surname,
+                //     age: this.age,
+                //     gender: this.selectedGender,
+                //     summary: this.summary
+                // }),
             })
-            const data = await response.json();
+            const data2 = await response.json();
         }
     }
 })
