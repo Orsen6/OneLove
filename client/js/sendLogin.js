cb748d13d80556ff8341993753ac45197ad9c046
Orsen6 <orsenkul@gmail.com>
2021-12-02 13:22:27 +0200

    babka

--- client/js/sendLogin.js
+++ client/js/sendLogin.js
@@ -1,3 +1,4 @@
+
 app = new Vue({
     el: "#app",
     data: {
@@ -5,26 +6,34 @@ app = new Vue({
         password: ""
     },
     methods: {
-        async getInfo(token) {
-            const hui = await fetch("http://localhost:5000/api/profile", {
-                method: "GET",
-                headers:  {"Authorization": `Bearer ${token}`}
-            })
-            window.location.href = 'OneLove/client/profile.html';
-        },
-        async sendLogin(e) {
-            e.preventDefault();
-            const postLogin = await fetch("http://localhost:5000/api/login", {
-                method: "POST",
+        sendlogin () {
+            axios.post("http://localhost:5000/api/login", {
                 headers: {"Content-Type": "application/json"},
                 body: JSON.stringify({
                     email: this.email,
                     password: this.password
                 })
             })
-            const loginData = await postLogin.json();
-            console.log(loginData);
-            getInfo(loginData.accessToken);
         }
+        // async getInfo(token) {
+        //     const hui = await fetch("http://localhost:5000/api/profile", {
+        //         method: "GET",
+        //         headers:  {"Authorization": `Bearer ${token}`}
+        //     })
+        // },
+        // async sendLogin(e) {
+        //     e.preventDefault();
+        //     const postLogin = await fetch("http://localhost:5000/api/login", {
+        //         method: "POST",
+        //         headers: {"Content-Type": "application/json"},
+        //         body: JSON.stringify({
+        //             email: this.email,
+        //             password: this.password
+        //         })
+        //     })
+        //     const loginData = await postLogin.json();
+        //     console.log(loginData);
+
+        // }
     }
 })
