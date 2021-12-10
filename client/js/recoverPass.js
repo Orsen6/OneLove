650f9acc32a50f6627fd19498156c172ae6603d3
Orsen6 <orsenkul@gmail.com>
2021-12-10 11:52:53 +0200

    4inaaa

--- client/js/recoverPass.js
+++ client/js/recoverPass.js
@@ -8,11 +8,15 @@ let app = new Vue({
             e.preventDefault();
             const response = await fetch("http://localhost:5000/api/sendmail", {
                 method: "POST",
-                headers: {"Content-Type": "application.json"},
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
                 body: JSON.stringify({
                     email: this.email
                 })
-            })
+            });
+            return response.json();
         }
     }
 })
